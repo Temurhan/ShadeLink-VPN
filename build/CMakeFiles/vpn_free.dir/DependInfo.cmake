4d0e460a3c765f68806dc2b9dbab10c6bcd97a28
Temurhan <temurhanreimbayew@gmail.com>
2025-12-24 14:28:14 -0500

    Revise README for improved clarity and organization
    Updated README.md to enhance clarity and structure, added sections for overview, key features, and installation instructions.

---
+++ build/CMakeFiles/vpn_free.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/devopstim/Documents/vpn_free/main.cpp" "CMakeFiles/vpn_free.dir/main.cpp.o" "gcc" "CMakeFiles/vpn_free.dir/main.cpp.o.d"
+  "/home/devopstim/Documents/vpn_free/src/core/EventManager.cpp" "CMakeFiles/vpn_free.dir/src/core/EventManager.cpp.o" "gcc" "CMakeFiles/vpn_free.dir/src/core/EventManager.cpp.o.d"
+  "/home/devopstim/Documents/vpn_free/src/core/TunInterface.cpp" "CMakeFiles/vpn_free.dir/src/core/TunInterface.cpp.o" "gcc" "CMakeFiles/vpn_free.dir/src/core/TunInterface.cpp.o.d"
+  "/home/devopstim/Documents/vpn_free/src/crypto/Encryptor.cpp" "CMakeFiles/vpn_free.dir/src/crypto/Encryptor.cpp.o" "gcc" "CMakeFiles/vpn_free.dir/src/crypto/Encryptor.cpp.o.d"
+  "/home/devopstim/Documents/vpn_free/src/network/SocketHandler.cpp" "CMakeFiles/vpn_free.dir/src/network/SocketHandler.cpp.o" "gcc" "CMakeFiles/vpn_free.dir/src/network/SocketHandler.cpp.o.d"
+  "" "vpn_free" "gcc" "CMakeFiles/vpn_free.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/vpn_free.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vpn_free.dir/link.d"
+  "CMakeFiles/vpn_free.dir/main.cpp.o"
+  "CMakeFiles/vpn_free.dir/main.cpp.o.d"
+  "CMakeFiles/vpn_free.dir/src/core/EventManager.cpp.o"
+  "CMakeFiles/vpn_free.dir/src/core/EventManager.cpp.o.d"
+  "CMakeFiles/vpn_free.dir/src/core/TunInterface.cpp.o"
+  "CMakeFiles/vpn_free.dir/src/core/TunInterface.cpp.o.d"
+  "CMakeFiles/vpn_free.dir/src/crypto/Encryptor.cpp.o"
+  "CMakeFiles/vpn_free.dir/src/crypto/Encryptor.cpp.o.d"
+  "CMakeFiles/vpn_free.dir/src/network/SocketHandler.cpp.o"
+  "CMakeFiles/vpn_free.dir/src/network/SocketHandler.cpp.o.d"
+  "vpn_free"
+  "vpn_free.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vpn_free.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
